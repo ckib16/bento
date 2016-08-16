9e8919585c29ebe23dd2321347ea4beeec61257b
ckib16 <ckib16@gmail.com>
2016-08-16 14:50:48 -0400

    Use full default xubuntu / XFCE desktop

--- apps/ubuntu-16.04/ror.sh
+++ apps/ubuntu-16.04/ror.sh
@@ -322,9 +322,13 @@ sudo sed -i '/Defaults\senv_reset/ a\Defaults\tenv_keep +="no_proxy"' /etc/sudoe
 # # especially for people on windows. what's another GB? or two? fiber ftw
 
 # install a lightweight desktop
-sudo apt-get -y install xubuntu-desktop --no-install-recommends
-sudo apt-get -y install xfce4-terminal
-sudo apt-get -y install xfce4-screenshooter xfce4-whiskermenu-plugin xfpanel-switch xfce4-taskmanager xfce4-quicklauncher-plugin xfce4-indicator-plugin ristretto mugshot libxfce4ui-utils file-roller xfce4-clipman-plugin xfce4-linelight-plugin thunar-archive-plugin xfce4-power-manager gtk3-engines-xfce
+sudo apt-get -y install xubuntu-desktop
+
+# Jay's original stripped-down XFCE install below
+# sudo apt-get -y install xubuntu-desktop --no-install-recommends
+# sudo apt-get -y install xfce4-terminal
+# sudo apt-get -y install xfce4-screenshooter xfce4-whiskermenu-plugin xfpanel-switch xfce4-taskmanager xfce4-quicklauncher-plugin xfce4-indicator-plugin ristretto mugshot libxfce4ui-utils file-roller xfce4-clipman-plugin xfce4-linelight-plugin thunar-archive-plugin xfce4-power-manager gtk3-engines-xfce
+
 sudo apt-get -y install qpdfview # pdf viewer
 
 # # # but not libreoffice
